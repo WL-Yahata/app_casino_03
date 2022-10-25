4097765a9d2dbd15ae994c2ee173688d93bfb205
WL-Yahata <106352826+WL-Yahata@users.noreply.github.com>
2022-10-25 17:34:04 +0900

    あ

---
+++ lib/view/ownersView/ownersSchedule/scheTournamentList.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ScheTournamentList extends StatefulWidget {
+  const ScheTournamentList({Key? key}) : super(key: key);
+
+  @override
+  State<ScheTournamentList> createState() => _ScheTournamentListState();
+}
+
+class _ScheTournamentListState extends State<ScheTournamentList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
